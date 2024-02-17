0cc0b40062007c3614f7ea97c76251622c5336f9
Nelly Lopez <nellymlopez3@gmail.com>
2024-02-17 08:16:59 -0800

    added functions, questions and inquirer prompt

---
+++ Develop/Sample.md
@@ -0,0 +1,58 @@
+# <Your-Project-Title>
+
+## Table of Contents
+
+- [Description](#project-description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Testing](#testing)
+  - [Questions](#questions)
+
+## Description
+
+<!-- Provide the description variable ${description} -->
+
+## Installation
+
+What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+
+<!-- Provide the  installation variable ${installation} -->
+
+## Usage
+
+<!-- Provide the  usage variable ${usage} -->
+
+Provide instructions and examples for use. Include screenshots as needed.
+
+To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+
+    ```md
+    ![alt text](assets/images/screenshot.png)
+    ```
+
+## Credits
+
+List your collaborators, if any, with links to their GitHub profiles.
+
+If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+
+If you followed tutorials, include links to those here as well.
+
+## Tests
+
+Go the extra mile and write tests for your application. Then provide examples on how to run them here.
+
+## License
+
+The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+
+---
+
+🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+
+## Questions
+
+Link to my Github Profile: https://github.com/${profile-name}
+
+You can reach me at : (email goes here)
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,130 @@
 // TODO: Include packages needed for this application
-
-// TODO: Create an array of questions for user input
-const questions = [];
-
+const { profile } = require("console");
+const fs = require("fs");
+const inquirer = require("inquirer");
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(file, data) {
+  fs.writeFile(file, data, (err) =>
+    err
+      ? console.log("Error occured when writing file")
+      : console.log("Success in creating your README.md file")
+  );
+}
+
+//generate the readme file
+const generateReadMe = ({
+  title,
+  description,
+  installation,
+  usage,
+  contribution,
+  test,
+  license,
+  github,
+  email,
+}) => {
+  return `# ${title}
+  ## Table of Contents 
+
+  - [Description](#project-description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Testing](#testing)
+  - [Questions](#questions)
+
+## Description
+
+${description}
+
+## Installation
+
+${installation}
+
+## Usage
+${usage}
+
+## Credits
+
+${contribution}
+
+## Tests
+
+${test}
+
+## License
+
+${license}
+
+## Questions
+
+Link to my Github Profile: https://github.com/${github}
+
+You can reach me at : ${email}
+`;
+};
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    type: "input",
+    message: "What is the name of your project?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Write a description of your project: ",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "What are the steps required to install your project?",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "Provide instructions and examples for use: ",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "List your collaborators, if any: ",
+    name: "contribution",
+  },
+  {
+    type: "input",
+    message:
+      "Go the extra mile and write tests for your application.Then provide examples on how to run them here. ",
+    name: "test",
+  },
+  {
+    type: "checkbox",
+    message: "What license do you want to use?",
+    name: "license",
+    choices: ["MIT", "Option1", "Option2", "Option3", "FOption4"],
+  },
+  {
+    type: "input",
+    message: "Enter your Github Profile name/alias (ex. johnSmith00): ",
+    name: "github",
+  },
+  {
+    type: "input",
+    message: "Enter your email address: ",
+    name: "email",
+  },
+];
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((data) => {
+    console.log(data);
+    //create a file with the name of the github on it
+    const name = ({ github }) => {
+      return github;
+    };
+    writeToFile(`${name(data)}_README.md`, generateReadMe(data));
+  });
+}
 
 // Function call to initialize app
 init();
